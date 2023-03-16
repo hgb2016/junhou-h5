c4620373fc53ee35ff63aae31339729103e48715
Hou Guo Bing <houguobing@tangbull.com>
2023-03-16 10:30:14 +0800

    fix store

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,7 @@
 import {defineStore} from "pinia";
 //参数1：定义一个仓库的唯一id名，Pinia会将所有的容器挂载到根容器；参数2：选项对象
-export const useStore =  defineStore('main',{
-  state:()=>{//state必须是函数，是避免在服务端渲染时的交叉请求导致的状态数据污染；必须是箭头函数，为了更好的TS类型推导
+export const useStore = defineStore('main', {
+  state: () => {
     return {
       // 登录信息
       loginData: null,
@@ -12,10 +12,10 @@ export const useStore =  defineStore('main',{
       // 当前账单
       currentBill: null,
       //是否
-      isLoading:false
+      isLoading: false
     }
   },
-  getters:{
+  getters: {
     //函数接受一个可选参数:state
 
     // count10(state){return state.count + 10}
@@ -23,7 +23,5 @@ export const useStore =  defineStore('main',{
       return this.count += 12
     }*/
   },
-  actions:{
-
-  }
+  actions: {}
 })
